ac01af2095572e9c5abac253b0571436dd68b015
Hamza Husein <65483354+Hamdrive@users.noreply.github.com>
2021-11-29 20:24:47 +0530

    [feat] create components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import "./styles.css";
+import Header from "./Header";
+import MemeContent from "./MemeContent";
+import GenerateMeme from "./GenerateMeme";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <Header />
+      <MemeContent />
+      <GenerateMeme />
     </div>
   );
 }
